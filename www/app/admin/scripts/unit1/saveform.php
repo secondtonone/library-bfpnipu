1662f28341ee8e5498730134d20e2575df14f1cd
secondtonone <fixer40@bk.ru>
2013-11-26 16:29:08 +0600

    Update saveform.php

--- www/app/admin/scripts/unit1/saveform.php
+++ www/app/admin/scripts/unit1/saveform.php
@@ -1,10 +1,50 @@
-﻿<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Документ без названия</title>
-</head>
+<?php
+session_start();
+require_once('../../../scripts/connect.php');
 
-<body>
-</body>
-</html>
+    $datevid = date("Y-m-d H:i:s");
+    $id_book = $_GET['id_book'];
+    $id_man = $_GET['id_man'];
+    $i = $_GET['i'];   
+    
+try {
+
+        
+        if ($i>0){  
+        for ($j=1;$j<=$i;$j++)
+            {
+        if (!empty($_GET['id_book'.$j.'']) && !empty($_GET['id_man'.$j.'']))
+        { 
+        $id_book = $_GET['id_book'.$j.''];
+        $id_man = $_GET['id_man'.$j.''];  
+        $vid = $dbh->prepare('INSERT INTO `vidacha`(`id_man`,`id_book`,`data_vozvrata`,`na_rukah`,`poterya`) VALUES (?,?,?,?,?)');
+        $vid->execute(array($id_man,$id_book,$datevid,"Yes","No");
+        $ost = $dbh->prepare('UPDATE `book` SET `ostatok`=`ostatok`-? WHERE `id_book` =?');
+        $ost->execute(array(1,$id_book ));
+        }else{
+           echo "Не заполнены строки в поле №".$j.""; 
+           exit; 
+        }
+            } 
+        echo "Записи добавлены!";
+        }
+        else 
+        {if (!empty($_GET['id_book']) && !empty($_GET['id_man']))
+        { 
+        $vid = $dbh->prepare('INSERT INTO `vidacha`(`id_man`,`id_book`,`data_vozvrata`,`na_rukah`,`poterya`) VALUES (?,?,?,?,?)');
+        $vid->execute(array($id_man,$id_book,$datevid,"Yes","No");
+        $ost = $dbh->prepare('UPDATE `book` SET `ostatok`=`ostatok`-? WHERE `id_book` =?');
+        $ost->execute(array(1,$id_book ));
+        echo "Запись добавлена!";
+        }
+        echo "Не заполнены строки в поле";
+        exit;
+        } 
+    
+       
+}  
+catch (PDOException $e) {
+    echo 'Database error: '.$e->getMessage();
+}
+
+?>
