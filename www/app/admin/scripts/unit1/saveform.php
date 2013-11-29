83d9cf10a3b7079d31a5aa2fb236cc77e35b7a9d
secondtonone <fixer40@bk.ru>
2013-11-29 10:39:25 +0600

    Update saveform.php

--- www/app/admin/scripts/unit1/saveform.php
+++ www/app/admin/scripts/unit1/saveform.php
@@ -8,6 +8,7 @@ require_once('../../../scripts/connect.php');
     $i = $_GET['i'];   
     
 try {
+	if ($i==0){
         if (!empty($_GET['id_book']) && !empty($_GET['id_man']))
         { 
         $vid = $dbh->prepare('INSERT INTO `vidacha`(`id_man`,`id_book`,`data_vozvrata`,`na_rukah`,`poterya`) VALUES (?,?,?,?,?)');
@@ -19,8 +20,7 @@ try {
         echo "Не заполнены строки в поле";
         exit;
         } 
-        
-      
+	}else{
         for ($j=1;$j<=$i;$j++)
             {
         if (!empty($_GET['id_book'.$j.'']) && !empty($_GET['id_man'.$j.'']))
@@ -34,8 +34,11 @@ try {
         $ost = $dbh->prepare('UPDATE `book` SET `ostatok`=`ostatok`-? WHERE `id_book` =?');
         $ost->execute(array(1,$id_book ));
         }else{
+           echo "Не заполнены строки в поле";
            exit; 
         }
+        }
+        echo "Записи добавлены";
         }
       
 }  
