74181ad49075d8d07e0d1eaf5e60b804d41aa3d2
secondtonone <fixer40@bk.ru>
2013-12-05 13:13:00 +0600

    Update new_pass_form.js

--- www/app/reg/forpass/new_pass_form.js
+++ www/app/reg/forpass/new_pass_form.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
                   type: "POST",  
                   url: "new_pass_controller.php",  
                   data: 'newpass='+ value1+'&rnewpass='+ value2+'&hideid='+ value3+'&code='+ value4,
-				  beforeSend: function() {$(".page").html("<img id='preloader' src='../../../img/preloader.gif'/>");}, 
+				  beforeSend: function() {$(".page").html("<div id='preloader'></div>");}, 
 				  success: function(data){  
                         $(".page").html("<div id='attentionForm'><a class='attentionText' href='../../user/index.php'>"+data+"</a></div>");
 						 setTimeout('window.location.href = "new_pass_form.php?id='+value3+'&&code='+value4+'"', 3000); 
