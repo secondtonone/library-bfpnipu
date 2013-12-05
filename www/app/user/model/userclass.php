c8d7fc4ed07f88c971b4f2aaf38a383c5b3dea9c
secondtonone <fixer40@bk.ru>
2013-12-05 19:07:15 +0800

    model

---
+++ www/app/user/model/userclass.php
@@ -0,0 +1,47 @@
+﻿<?php 
+
+class UserClass {
+	
+      public $data;
+	  
+	  	
+	function UserClass(){
+		$this->data="";
+		}
+		
+	public function check_data ($string){
+	$this->data=$string;
+	$this->data=trim($this->data);
+    $this->data=htmlspecialchars($this->data);
+	return $this->data;
+	}
+	
+	function get_data ($query,$data,$dbh){
+    $this->data=$dbh->prepare($query);
+    $this->data->execute(array($data));
+    $this->data=$this->data->fetch(PDO::FETCH_ASSOC);
+	return $this->data;
+	}
+	
+	function update_user($query,$updatequery,$data,$data2,$id,$dbh){
+	$this->data=$dbh->prepare($updatequery);
+    $this->data->execute(array($data,$data2,$id));
+	$this->data=$this->check_data($query,$id,$dbh);
+	return $this->data;
+	}	
+	
+	function update_ppl($query,$updatequery,$data,$data2,$data3,$data4,$id,$dbh){
+	$this->data=$dbh->prepare($updatequery);
+    $this->data->execute(array($data,$data2,$data3,$data4,$id));
+	$this->data=$this->check_data($query,$id,$dbh);
+	return $this->data;
+	}	
+	
+    
+	function __destruct() {
+         }
+	
+
+      
+}
+?>
