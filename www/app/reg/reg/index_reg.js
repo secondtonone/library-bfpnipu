29a347c9941e7a9fa3c6d04af23cdfd58c444a7b
secondtonone <fixer40@bk.ru>
2013-12-06 14:36:42 +0600

    Update index_reg.js

--- www/app/reg/reg/index_reg.js
+++ www/app/reg/reg/index_reg.js
@@ -14,9 +14,13 @@ $(document).ready(function(){
                   url: "reg_controller.php",  
                   data: 'login='+ value1+'&pass='+ value2+ '&pass2=' +value3+'&email='+ value4+ '&fam=' + value5+  '&number_zach=' + value6,
 				  beforeSend: function() {$(".page").html("<div id='preloader'></div>");},                 
-				   success: function(data){  
+				   success: function(data){ if (data=="Спасибо за регистрацию! Ваш аккаунт был успешно создан! Вам на email было выслано письмо с инструкциями для завершения регистрации!"){
+				   	
+		$(".page").html("<div id='attentionForm'><a class='attentionText' href='../../../user/index.php'>"+data+"</a></div>");
+		setTimeout('window.location.href = "../../../user/index.php"', 3000);		   }
+				   }else{ 
                         $(".page").html("<div id='attentionForm'><a class='attentionText' href='index_reg.php'>"+data+"</a></div>");
-						 setTimeout('window.location.href = "index_reg.php"', 3000);
+		setTimeout('window.location.href = "index_reg.php"', 3000);}
                     }  
                 });  
                 return false;  
