3e4a4845721ca6479a6cb8c718f44496343848e9
secondtonone <fixer40@bk.ru>
2014-01-23 12:15:14 +0500

    mail improve

--- www/app/mail/createtable.php
+++ www/app/mail/createtable.php
@@ -16,7 +16,7 @@ $query->execute();
 while($row = $query->fetch(PDO::FETCH_ASSOC)) 
      {
 
-	$res = $dbh->prepare('SELECT `name_book` FROM `book` as b INNER JOIN `vidacha` as v  ON v.`id_book`=b.`id_book` INNER JOIN `users` as u ON v.`id_man`=u.`id_man` WHERE `na_rukah`="Yes" AND `poterya`="No" AND ((YEAR(CURDATE())-YEAR(data_vidachi)>=1) OR (YEAR(CURDATE())-YEAR(`data_vidachi`)=1) OR (YEAR(CURDATE())-YEAR(data_vidachi)=0 AND MONTH(CURDATE())>6 AND MONTH(`data_vidachi`)<=5)) AND v.id_man=?');
+	$res = $dbh->prepare('SELECT `name_book` FROM `book` as b INNER JOIN `vidacha` as v  ON v.`id_book`=b.`id_book` INNER JOIN `people` as p ON v.`id_man`=p.`id_man` WHERE `na_rukah`="Yes" AND `poterya`="No" AND ((YEAR(CURDATE())-YEAR(data_vidachi)>=1) OR (YEAR(CURDATE())-YEAR(`data_vidachi`)=1) OR (YEAR(CURDATE())-YEAR(data_vidachi)=0 AND MONTH(CURDATE())>6 AND MONTH(`data_vidachi`)<=5)) AND v.id_man=?');
 	$res->execute(array($row["id_man"]));
 		 while($rs = $res->fetch(PDO::FETCH_ASSOC)) 
 	 {
